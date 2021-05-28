71974fdbd5186c41e4c05291de466d858d49c05a
transonhy96 <transonhy96@gmail.com>
2021-05-28 21:32:01 +0700

    remove Token

--- src/utils/getUrl.js
+++ src/utils/getUrl.js
@@ -1,21 +1,22 @@
+import apiKey from "@/apiKey.json";
 export default {
     getUrl:(networkID,address) =>{
         switch (networkID) {
             //Eth mainnet
             case 1:
-                return "https://api.etherscan.io/api?module=account&action=txlist&address="+address+"&startblock=0&endblock=99999999&page=1&offset=10&sort=asc&apikey=45QCR35X7V6I5PWIBGKC2AA7Y8HSV1AY4M";
+                return "https://api.etherscan.io/api?module=account&action=txlist&address="+address+"&startblock=0&endblock=99999999&page=1&offset=10&sort=asc&apikey="+apiKey.Key;
                 //Kovan
             case 42:
                 return "";
                 //Ropsten
             case 3:
-                return "https://api-ropsten.etherscan.io/api?module=account&action=txlist&address="+address+"&startblock=0&endblock=99999999&page=1&offset=10&sort=asc&apikey=45QCR35X7V6I5PWIBGKC2AA7Y8HSV1AY4M";
+                return "https://api-ropsten.etherscan.io/api?module=account&action=txlist&address="+address+"&startblock=0&endblock=99999999&page=1&offset=10&sort=asc&apikey="+apiKey.Key;
                 //BSC Test net
             case 97:
-                return "https://api-testnet.bscscan.com/api?module=account&action=txlist&address="+address+"&startblock=0&endblock=99999999&page=1&offset=10&sort=asc&apikey=45QCR35X7V6I5PWIBGKC2AA7Y8HSV1AY4M";  
+                return "https://api-testnet.bscscan.com/api?module=account&action=txlist&address="+address+"&startblock=0&endblock=99999999&page=1&offset=10&sort=asc&apikey="+apiKey.Key;  
                 //BSC mainnet
             case 56:
-                return "https://api.bscscan.com/api?module=account&action=txlist&address="+address+"&startblock=0&endblock=99999999&page=1&offset=10&sort=asc&apikey=45QCR35X7V6I5PWIBGKC2AA7Y8HSV1AY4M";
+                return "https://api.bscscan.com/api?module=account&action=txlist&address="+address+"&startblock=0&endblock=99999999&page=1&offset=10&sort=asc&apikey="+apiKey.Key;
             default:
                 //UE4 contract address
                 return "0x1148d983adc0f30f9d47e1fcb5346ce06f4f1b68";
